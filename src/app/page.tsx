2b4be9f8dd24a43442b59a4110977498741c8998
carlos <carlosseduardo003@hotmail.com>
2024-06-26 20:55:36 -0300

    [feat] - criando tela home e arrumando apresentação de games

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/container";
 import { GameProps } from '@/utils/types/games'
+import { BsArrowRightSquare } from 'react-icons/bs'
 
 import Image from "next/image";
 import Link from 'next/link'
@@ -7,7 +8,9 @@ import Link from 'next/link'
 async function getDalyGame() {
 
   try {
-    const requisitionGetGame = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const requisitionGetGame = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {
+      next: {revalidate: 320}
+    })
 
     return requisitionGetGame.json()
   } catch (error) {
@@ -28,14 +31,21 @@ export default async function Home() {
         </h1>
         <Link href={`/games/${dalyGame.id}`}>
           <div className="w-full bg-black rounded-lg">
-            <Image
-              src={dalyGame.image_url}
-              alt={dalyGame.title}
-              priority={true}
-              quality={100}
-              width={100}
-              height={100}
-            />
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF"/>
+              </div>
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width:1200px) 33vw"
+              />
+            </div>
           </div>
         </Link>
       </Container>
