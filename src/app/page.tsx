d9ce14fdefa618cea5fc1e85d6f60ba0b0149a08
carlos <carlosseduardo003@hotmail.com>
2024-06-25 21:30:22 -0300

    [feat] - criando tela home requisição api já sendo feita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
+import { Container } from "@/components/container";
+import { GameProps } from '@/utils/types/games'
+
+import Image from "next/image";
+import Link from 'next/link'
+
+async function getDalyGame() {
+
+  try {
+    const requisitionGetGame = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+
+    return requisitionGetGame.json()
+  } catch (error) {
+    throw new Error("Failed to fetch data")
+  }
+  
+}
+
+export default async function Home() {
+  const dalyGame:GameProps = await getDalyGame()
+
+  console.log(dalyGame)
   return (
-    <main>
-      <h1>teste</h1>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo para você
+        </h1>
+        <Link href={`/games/${dalyGame.id}`}>
+          <div className="w-full bg-black rounded-lg">
+            <Image
+              src={dalyGame.image_url}
+              alt={dalyGame.title}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+            />
+          </div>
+        </Link>
+      </Container>
     </main>
   );
 }
